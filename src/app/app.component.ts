a389c24b486608a88caf1ee024fed5f0c12a96f5
Szymon Grzelak <szymong333@gmail.com>
2017-05-27 12:30:57 +0200

    Starter for Angular 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent  { name = 'Angular 4, it\'s nice to meet you!'; }
---
+++ src/vendor.ts
@@ -0,0 +1,12 @@
+// Angular
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/forms';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+
+// You can import more deps like @angular/http or @angular/router
+
+// RxJS
+import 'rxjs';
