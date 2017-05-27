1d4c65ab80894686b8646bda0b5c737c9279b595
Szymon Grzelak <szymong333@gmail.com>
2017-05-27 16:09:50 +0200

    Prod env config file for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-module.exports = require('./config/webpack.dev.js');
+module.exports = function(env) {
+    if(env == undefined) {
+        console.log('Env variable is undefined, use dev config!')
+        env = 'dev';
+    } else {
+        console.log(`Building for ${env} environment.`)
+    }
+    return require(`./config/webpack.${env}.js`);
+}
